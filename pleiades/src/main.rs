350193ba3ca8022535b7a2c74d2f47a9f2ae4e9d
exor2008 <exor2008@yandex.ru>
2024-06-28 02:51:25 +0300

    Comments for main.rs

--- pleiades/src/main.rs
+++ pleiades/src/main.rs
@@ -37,25 +37,33 @@ static CHANNEL: Channel<ThreadModeRawMutex, Command, 1> = Channel::new();
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
     info!("Start");
+
+    // Init pins
     let p = embassy_rp::init(Default::default());
     let sda = p.PIN_20;
     let scl = p.PIN_21;
 
+    // Init I2C and Apds9960 gesture sensor 
     let i2c = i2c::I2c::new_async(p.I2C0, scl, sda, Irqs, Config::default());
     let apds = Apds9960::new(i2c);
 
+    // Start sensor_task asynchronously
     unwrap!(spawner.spawn(sensor_task(apds)));
 
+    // Init PIO to support WS2812 protocol
     let Pio {
         mut common, sm0, ..
     } = Pio::new(p.PIO0, Irqs);
 
+    Init WS2812 LED controller
     let mut ws2812: Ws2812<PIO0, STATE_MACHINE, NUM_LEDS> =
         Ws2812::new(&mut common, sm0, p.DMA_CH0, p.PIN_22);
 
+    // Init 16x16 LED matrix controller
     let mut led_matrix: LedMatrix<Ws2812<PIO0, 0, NUM_LEDS>, NUM_LEDS_LINE, NUM_LEDS> =
         LedMatrix::new(&mut ws2812);
 
+    // Create a new world
     let mut world: World<'_, _, NUM_LEDS_COLUMN, NUM_LEDS_LINE, NUM_LEDS, { 2 * NUM_LEDS }> =
         World::fire_new(&mut led_matrix);
     // > = World::matrix_from(ws2812);
@@ -65,6 +73,7 @@ async fn main(spawner: Spawner) {
     let mut switch = Switch::new();
 
     loop {
+        // Handle the command from the gesture sensor
         if let Ok(command) = CHANNEL.try_receive() {
             // defmt::info!("Command!: {}", command);
             match command {
